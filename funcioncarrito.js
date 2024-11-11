917997bfa13e514118cdf31864c5eb2bf8d09596
Judith-curiel <judithmontelongocuriel10@gmail.com>
2024-11-11 16:05:52 -0600

    Add files via upload

---
+++ funcioncarrito.js
@@ -0,0 +1,5 @@
+ function myFunction() {
+                document.getElementById('demo2').style.fontSize = '50px';
+                document.getElementById("demo2").innerHTML = "$2,599.00";
+                document.getElementById("demo2").style.color = "orange";
+            }
